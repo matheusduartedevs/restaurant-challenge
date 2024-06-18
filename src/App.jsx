349c46d99a6bfb068864c999e017a6a1f6745de8
unknown <matheus.duartesilva42@gmail.com>
2024-06-18 19:31:48 -0300

    feat: implementando o hook context na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Header from "./components/Header/Header"
-import Products from "./components/Products/Products"
+import React from 'react'
+import Header from './components/Header/Header'
+import Products from './components/Products/Products'
+import Provider from './context/Provider'
+import Cart from './components/Cart/Cart'
 
 const App = () => {
   return (
-    <div>
+    <Provider>
       <Header />
       <Products />
-    </div>
+      <Cart />
+    </Provider>
   )
 }
 
